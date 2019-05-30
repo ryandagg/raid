d09f21555092e8099266285d19910029f6fdcffa
ryandagg <ryan.dagg@gmail.com>
2019-05-30 10:48:41 -0600

    Exposing guts sufficiently to run the client from another repo.

---
+++ index.js
@@ -0,0 +1,12 @@
+// game components
+var GameRunner = require('./lib/GameRunner');
+var GameConstants = require('./lib/GameConstants');
+var UnitTypes= require('./lib/UnitType');
+
+
+
+modules.export = {
+    GameRunner,
+    GameConstants,
+    UnitTypes,
+}
